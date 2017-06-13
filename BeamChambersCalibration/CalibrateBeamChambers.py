032ea0daea7a7f880df436d084a9c5f5575e90f1
ATLAS Tile Demonstrator <atlas.tiledemo@cern.ch>
2017-06-13 14:29:10 +0200

    Updated BC calibration scripts

--- BeamChambersCalibration/CalibrateBeamChambers.py
+++ BeamChambersCalibration/CalibrateBeamChambers.py
@@ -1,47 +1,46 @@
 #! /usr/bin/env python
 
+import os
 import sys
 import argparse
-
 import numpy as np
 
 from ROOT import gROOT, kGray, gStyle, TColor, TFile, TChain, TH1F, TH2F, TCanvas, TGraph, TF1
 from root_numpy import root2array, tree2array, fill_hist
 
-
-# Joakim Olsson, UChicago (joakim.olsson@cern.ch)
-# Created 2016-10-22
+# Joakim Olsson (joakim.olsson@cern.ch)
+# 2016-10-22
 
 # Usage:
-# python CalibrateBeamChambers.py -bc BC_NUMBER -r LEFT_UP_CALIB_RUN CENTER_CALIB_RUN RIGHT_DOWN_CALIB_RUN --plots
-#
+# python CalibrateBeamChambers.py --beam-chamber <bc_number> -l <left_up_run> -c <center_run> -r <right_down_run> --plots
+
 # Example:
-# python CalibrateBeamChambers.py -bc 1 -r 611274 611276 611278 --path tiletb_September2016 --plots
+# python CalibrateBeamChambers.py --beam-chamber 1 -l 611274 -c 611276 -r 611278 --input-path tiletb_September2016 --output_path test --plots
 
 def main():
 
     # Parse input arguments
-    parser = argparse.ArgumentParser(description = 'Perform beam chamber calibration')
-    parser.add_argument('-bc', '--beam_chamber', dest='bc', required=True, type=int, choices=[1,2],
-                        default = 1, help='Specify the beam chamber to calibrate')
-    parser.add_argument('-r', '--runs', dest='runs', nargs=3, required=True, type=int,
-                        default = [611274, 611276, 611278], help='List of calibration runs to process (LEFT_UP CENTER RIGHT_DOWN')
-    parser.add_argument('-n', '--nevents', dest='nevents', type=int,
-                        default = -1, help='Number of events to process')
-    parser.add_argument('--plots', action = 'store_true', default = False, help='Save calibration plots')
+    parser = argparse.ArgumentParser(description = 'Perform beam chamber calibration (Author: Joakim Olsson)')
+    parser.add_argument('--beam-chamber', dest='bc', required=True, type=int, choices=[1,2], default = 1, help='Specify which beam chamber to calibrate')
+    parser.add_argument('-l', '--left-up', dest='runs_lu', required=True, type=int, nargs='+', metavar='<left_up_run_number>', help='Calibration run(s) for left-up')
+    parser.add_argument('-c', '--center', dest='runs_c', required=True, type=int, nargs='+', metavar='<center_run_number>', help='Calibration run(s) for center')
+    parser.add_argument('-r', '--right-down', dest='runs_rd', required=True, type=int, nargs='+', metavar='<right_down_run_number>', help='Calibration run(s) for right-down')
+    parser.add_argument('-n', '--nevents', dest='nevents', type=int, default = -1, help='Number of events to process')
+    parser.add_argument('-p', '--plots', action = 'store_true', default = False, help='Save calibration plots')
+    parser.add_argument('-i', '--input-path', dest='input_path', default='./', help='Path to input files')
+    parser.add_argument('-o', '--output-path', dest='output_path', default='./', help='Path to output files')
+    parser.add_argument('-t', '--tree', dest='treename', default='h1000', help='Name of input TTree')
     parser.add_argument('--test_calib', action = 'store_true', default = False, help='Test calibration')
-    parser.add_argument('-p', '--path', dest='input_path', default='./',
-                        help='Path to input files')
-    parser.add_argument('-t', '--tree', dest='treename', default='h1000',
-                        help='Name of input TTree')
     parser.add_argument('--verbose', '-v', action='count')
 
     if len(sys.argv) < 2:
         parser.print_help()
-        print('\nexample: python CalibrateBeamChambers.py -bc 1 -r 611274 611276 611278 --path tiletb_September2016 --plots\n')
+        print('\nEx. python CalibrateBeamChambers.py -bc 1 -lu 611274 -c 611276 -rd 611278 --input-path tiletb_September2016 --plots\n')
         sys.exit(1)
 
     args = parser.parse_args()
+
+    ensure_dir(args.output_path)
 
     debug = 0
     if (args.verbose > 0):
@@ -53,31 +52,38 @@ def main():
 
     else:
 
-        # calibration of BC
-        pos_leftup = get_calib_pos(args.runs[0], args.bc, args.nevents, args.treename,
-                                   args.input_path, args.plots, debug)
-        pos_center = get_calib_pos(args.runs[1], args.bc, args.nevents, args.treename,
-                                   args.input_path, args.plots, debug)
-        pos_rightdown = get_calib_pos(args.runs[2], args.bc, args.nevents, args.treename,
-                                      args.input_path, args.plots, debug)
+        if (args.bc < 1 or args.bc > 2):
+            print('ERROR: Invalid BC#, must be either 1 or 2')
+
+        if (debug == 2): print 'Running calibration for BC{}'.format(args.bc)
+
+        # get the positions for left/up, center, right/down
+        pos_leftup = get_pos(args.runs_lu, args.bc, args.nevents, args.treename, args.input_path, args.output_path, args.plots, debug)
+        pos_center = get_pos(args.runs_c, args.bc, args.nevents, args.treename, args.input_path, args.output_path, args.plots, debug)
+        pos_rightdown = get_pos(args.runs_rd, args.bc, args.nevents, args.treename, args.input_path, args.output_path, args.plots, debug)
 
         if debug:
             print '\n----> Calibration positions'
-            print 'Left/Up (run {}):'.format(args.runs[0])
+            print 'Left/Up (runs {}):'.format(args.runs_lu)
             print pos_leftup
-            print 'Center (run {}):'.format(args.runs[1])
+            print 'Center (runs {}):'.format(args.runs_c)
             print pos_center
-            print 'Right/Down (run {}):'.format(args.runs[2])
+            print 'Right/Down (runs {}):'.format(args.runs_rd)
             print pos_rightdown
 
-        runs_tag = ''
+        # label outputs based on run numbers
+        runs_tag = 'lu'
+        for run in args.runs_lu:
+            runs_tag += '_{:d}'.format(run)
+        runs_tag = '_c'
+        for run in args.runs_c:
+            runs_tag += '_{:d}'.format(run)
+        runs_tag = '_rd'
+        for run in args.runs_rd:
+            runs_tag += '_{:d}'.format(run)
 
-        for i,run in enumerate(args.runs):
-            if (i < len(args.runs)-1):
-                runs_tag += '{:d}_'.format(run)
-            else:
-                runs_tag += '{:d}'.format(run)
-        calib = get_calib(pos_leftup, pos_center, pos_rightdown, args.bc, runs_tag, args.plots, debug)
+        # calibrate based on the positions obtained above
+        calib = calibrate(pos_leftup, pos_center, pos_rightdown, args.bc, runs_tag, args.plots, args.output_path, debug)
 
         print '\n----> Results of the calibration for BC{}\n'.format(args.bc)
         print 'Horizontal slope = {0:2f}'.format(float(calib['x_slope']))
@@ -85,26 +91,19 @@ def main():
         print 'Vertical slope = {}'.format(calib['y_slope'])
         print 'Vertical offset = {}\n'.format(calib['y_offset'])
 
-def get_calib_pos(run, bc = 1, nevents = -1, treename = 'h1000', input_path = './', save_plots = False, debug = 0):
-
-    if (bc < 1 or bc > 2):
-        print('ERROR: Invalid BC#, must be either 1 or 2')
-
-    # mapping : BC1 : left = 0, right = 1, up = 2, down = 3
-    #           BC2 : left = 4, right = 5, up = 6, down = 7
-    l = 0; r = 1; u = 2; d = 3
-    if bc == 2:
-        l = 4; r = 5; u = 6; d = 7
-    if debug == 2:
-        print 'Running calibration for BC{}'.format(bc)
+def get_pos(runs, bc=1, nevents = -1, treename = 'h1000', input_path = './', output_path = './', save_plots = False, debug = False, cut_min = 0, cut_max = 10000):
 
     chain = TChain(treename)
-    chain.Add(input_path+'/tiletb_{}.root'.format(run))
+    for run in runs:
+        chain.Add(input_path+'/tiletb_{}.root'.format(run))
+
+    # mapping of beam chamber outputs (from the TDC)
+    # BC1 : left = 0, right = 1, up = 2, down = 3
+    # BC2 : left = 4, right = 5, up = 6, down = 7
+    tdc = {'l':0, 'r':1, 'u':2, 'd':3}
+    if (bc == 2): tdc = {'l':4, 'r':5, 'u':6, 'd':7}
 
     bc_array = tree2array(chain, branches=['btdc1'], start=0, stop=nevents)
-
-    cut_min = 0
-    cut_max = 10000
 
     if save_plots:
        h = HistogramManager('run_{}_bc_{}'.format(run, bc))
@@ -119,10 +118,10 @@ def get_calib_pos(run, bc = 1, nevents = -1, treename = 'h1000', input_path = '.
     for i_i, bc_pos_i in np.ndenumerate(bc_array):
         i = i_i[0]
         bc_pos = bc_pos_i[0]
-        sumx = bc_pos[r]+bc_pos[l]
-        sumy = bc_pos[u]+bc_pos[d]
-        deltax_tmp = bc_pos[r]-bc_pos[l]
-        deltay_tmp = bc_pos[u]-bc_pos[d]
+        sumx = bc_pos[tdc['r']]+bc_pos[tdc['l']]
+        sumy = bc_pos[tdc['u']]+bc_pos[tdc['d']]
+        deltax_tmp = bc_pos[tdc['r']]-bc_pos[tdc['l']]
+        deltay_tmp = bc_pos[tdc['u']]-bc_pos[tdc['d']]
         if save_plots:
             h.fill_1d('sumx', sumx)
             h.fill_1d('sumy', sumy)
@@ -162,7 +161,7 @@ def get_calib_pos(run, bc = 1, nevents = -1, treename = 'h1000', input_path = '.
         h.hists['deltax'].Draw('hist')
         c1.cd(4)
         h.hists['deltay'].Draw('hist')
-        c1.SaveAs('calibration_pos_bc_{}_run_{}.pdf'.format(bc, run))
+        c1.SaveAs(os.path.join(output_path, 'calibration_pos_bc_{}_run_{}.pdf'.format(bc, run)))
         c2 = TCanvas()
         c2.SetLeftMargin(0.12)
         c2.SetRightMargin(0.15)
@@ -174,11 +173,11 @@ def get_calib_pos(run, bc = 1, nevents = -1, treename = 'h1000', input_path = '.
         c2.SetTicky()
         h.hists['deltay_vs_deltax'].SetTitle('')
         h.hists['deltay_vs_deltax'].Draw('colz')
-        c2.SaveAs('calibration_pos_deltay_vs_deltax_bc_{}_run_{}.pdf'.format(bc, run))
+        c2.SaveAs(os.path.join(output_path, 'calibration_pos_deltay_vs_deltax_bc_{}_run_{}.pdf'.format(bc, run)))
 
     return { 'x' : deltax_avg, 'xerr' : deltax_std, 'y' : deltay_avg, 'yerr' : deltay_std }
 
-def get_calib(pos_leftup, pos_center, pos_rightdown, bc, runs_tag, save_plots = False, debug = 0):
+def calibrate(pos_leftup, pos_center, pos_rightdown, bc, runs_tag, save_plots = False, output_path = './', debug = 0):
 
     fix_pts_x = np.array([-30, 0, 30])
     fix_pts_y = np.array([30, 0, -30])
@@ -237,7 +236,7 @@ def get_calib(pos_leftup, pos_center, pos_rightdown, bc, runs_tag, save_plots = 
                 xy = (xi, yi), xytext = (20, 20),
                 textcoords = 'offset points', ha = 'right', va = 'bottom',
                 bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5))
-        plt.savefig('calibration_fit_bc_{:d}_runs_{}.pdf'.format(bc, runs_tag))
+        plt.savefig(os.path.join(output_path, 'calibration_fit_bc_{:d}_runs_{}.pdf'.format(bc, runs_tag)))
 
     return {'x_offset': x_fit[0], 'x_slope': x_fit[1],
             'y_offset': y_fit[0], 'y_slope': y_fit[1]}
@@ -327,6 +326,10 @@ class HistogramManager:
 
     def nhists(self):
         return len(self.hists)
+
+def ensure_dir(d):
+    if not os.path.exists(d):
+        os.makedirs(d)
 
 if __name__ == '__main__':
     main()
